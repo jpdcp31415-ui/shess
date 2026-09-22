7ee30ccc585cbc0ca553dd858c14f32b6791be74
janni <jpdcp31415-ui@github.com>
2026-09-22 17:50:15 +0100

    fixed the win bug

--- src/game-state.c
+++ src/game-state.c
@@ -93,7 +93,7 @@ bool isWinForOppPlayer(const ChessGame* game)
     if (canBlockCheck(game)) return false;
 
     const ChessMove attack = getAttackToKing(game);
-    if (canCaptureKingAttacker(game, &attack)) return false;
+    if (!currPieceMayMoveTo(game, &attack.boardMove.position, false)) return false;
 
     return true;
 }
